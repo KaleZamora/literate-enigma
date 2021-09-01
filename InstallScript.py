a983cea5e295cd63c537143cd1e03d1efed81bd9
Zachary Knight <61021511+KaleZamora@users.noreply.github.com>
2021-09-01 18:26:48 -0400

    Add files via upload

--- InstallScript.py
+++ InstallScript.py
@@ -1,181 +1,66 @@
-import os
-import subprocess
+import definitions
 
 print("Welcome to the automated installer, please select an option:\n"
       " 1. Install Office Only\n 2. Install Connectwise Only\n 3. Install BitDefender Only\n 4. Install Applications "
       "Only\n 5. Install All\n 6. Install Connectwise and BitDefender only\n 7. Quit\n 9. Install Applications and Office only")
-value = input("What is your selection: ")
-
+value = input("What is your selection: ")    
+    
 if value == "1":
-      os.system(r"C:\Util\Project\setup.exe /configure C:\UTIL\Project\Configuration_test.xml")
+    definitions.office_install()
 
 elif value == "2":
-      inst = False
-      folder = "ScreenConnect"
-      for root in os.walk(r"C:\Program Files (x86)"):
-          for name in root:
-              if folder not in name:
-                  continue
-              else:
-                  inst = True
-      if inst == False:
-            os.system(r"msiexec /i C:\UTIL\Project\Agent_Install.msi /qb")
+    definitions.agent_install()    
 
 elif value == "3":
-      folder2 = "Bitdefender"
-      inst = False
-      for root in os.walk(r"C:\Program Files"):
-          for name in root:
-              if folder2 not in name:
-                  continue
-              else:
-                  inst = True
-      if inst == False:
-            os.system(r"msiexec /i C:\UTIL\Project\eps_installer_signed.msi /qb GZ_PACKAGE_ID=aHR0cHM6Ly9jbG91ZC1lY3MuZ3Jhdml0eXpvbmUuYml0ZGVmZW5kZXIuY29tL1BhY2thZ2VzL0JTVFdJTi8wL0RlUkQ0WS9pbnN0YWxsZXIueG1sP2xhbmc9ZW4tVVM=")
+    definitions.bitdefender_install()
 
 elif value == "4":
-      output_arr = []
-      output_arr.append(subprocess.check_output(r"powershell.exe get-appxpackage Microsoft.VCLibs.140.00.UWPDesktop", shell=True))
-      output_arr.append(subprocess.check_output(r"powershell.exe get-appxpackage Microsoft.VCLibs.140.00", shell=True))
-
-      for i in output_arr:
-            if not i.decode('utf-8'):
-                  subprocess.call(r"powershell.exe Add-AppPackage -Path 'C:\UTIL\Project\Microsoft.VCLibs.x64.14.00.Desktop.appx'", shell=True)
-
-      subprocess.call(r"powershell.exe Add-AppPackage -Path 'C:\UTIL\Project\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'", shell=True)
-      subprocess.call(r"powershell.exe winget import --import-file 'C:\UTIL\Project\winstall-9138.json'", shell=True)
+    definitions.applications_install()
       
 elif value == "5":
-      output_arr = []
-      output_arr.append(subprocess.check_output(r"powershell.exe get-appxpackage Microsoft.VCLibs.140.00.UWPDesktop", shell=True))
-      output_arr.append(subprocess.check_output(r"powershell.exe get-appxpackage Microsoft.VCLibs.140.00", shell=True))
-
-      for i in output_arr:
-            if not i.decode('utf-8'):
-                  subprocess.call(r"powershell.exe Add-AppPackage -Path 'C:\UTIL\Project\Microsoft.VCLibs.x64.14.00.Desktop.appx'", shell=True)
-
-      subprocess.call(r"powershell.exe Add-AppPackage -Path 'C:\UTIL\Project\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'", shell=True)
-      subprocess.call(r"powershell.exe winget import --import-file 'C:\UTIL\Project\winstall-9138.json'", shell=True)
-      inst = False
-      folder = "ScreenConnect"
-      for root in os.walk(r"C:\Program Files (x86)"):
-          for name in root:
-              if folder not in name:
-                  continue
-              else:
-                  inst = True
-      if inst == False:
-            os.system(r"msiexec /i C:\UTIL\Project\Agent_Install.msi /qb")
-      folder2 = "Bitdefender"
-      inst = False
-      for root in os.walk(r"C:\Program Files"):
-          for name in root:
-              if folder2 not in name:
-                  continue
-              else:
-                  inst = True
-      if inst == False:
-            os.system(r"msiexec /i C:\UTIL\Project\eps_installer_signed.msi /qb GZ_PACKAGE_ID=aHR0cHM6Ly9jbG91ZC1lY3MuZ3Jhdml0eXpvbmUuYml0ZGVmZW5kZXIuY29tL1BhY2thZ2VzL0JTVFdJTi8wL0RlUkQ0WS9pbnN0YWxsZXIueG1sP2xhbmc9ZW4tVVM=")
-      os.system(r"C:\Util\Project\setup.exe /configure C:\UTIL\Project\Configuration_test.xml")
+    definitions.applications_install()
+    definitions.agent_install()
+    definitions.bitdefender_install()
+    definitions.office_install()
       
 elif value == "6":
-      inst = False
-      folder = "ScreenConnect"
-      for root in os.walk(r"C:\Program Files (x86)"):
-          for name in root:
-              if folder not in name:
-                  continue
-              else:
-                  inst = True
-      if inst == False:
-            os.system(r"msiexec /i C:\UTIL\Project\Agent_Install.msi /qb")
-      folder2 = "Bitdefender"
-      inst = False
-      for root in os.walk(r"C:\Program Files"):
-          for name in root:
-              if folder2 not in name:
-                  continue
-              else:
-                  inst = True
-      if inst == False:
-            os.system(r"msiexec /i C:\UTIL\Project\eps_installer_signed.msi /qb GZ_PACKAGE_ID=aHR0cHM6Ly9jbG91ZC1lY3MuZ3Jhdml0eXpvbmUuYml0ZGVmZW5kZXIuY29tL1BhY2thZ2VzL0JTVFdJTi8wL0RlUkQ0WS9pbnN0YWxsZXIueG1sP2xhbmc9ZW4tVVM=")
-
+    definitions.agent_install()
+    definitions.bitdefender_install()
+    
 elif value == "7":
-      print("Goodbye!")
-      exit()
+    print("Goodbye!")
+    exit()
       
 elif value == "8":
-      print("testing network setup")
+    print("testing network setup")
 
 elif value == "9":
-      output_arr = []
-      output_arr.append(subprocess.check_output(r"powershell.exe get-appxpackage Microsoft.VCLibs.140.00.UWPDesktop", shell=True))
-      output_arr.append(subprocess.check_output(r"powershell.exe get-appxpackage Microsoft.VCLibs.140.00", shell=True))
-
-      for i in output_arr:
-            if not i.decode('utf-8'):
-                  subprocess.call(r"powershell.exe Add-AppPackage -Path 'C:\UTIL\Project\Microsoft.VCLibs.x64.14.00.Desktop.appx'", shell=True)
-
-      subprocess.call(r"powershell.exe Add-AppPackage -Path 'C:\UTIL\Project\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'", shell=True)
-      subprocess.call(r"powershell.exe winget import --import-file 'C:\UTIL\Project\winstall-9138.json'", shell=True)
-      os.system(r"C:\Util\Project\setup.exe /configure C:\UTIL\Project\Configuration_test.xml")
+    definitions.applications_install()
+    definitions.office_install()
       
 else:
       print("You have chosen an invalid number, please relaunch exe and try again")
       exit()
-print('Configuring network settings')
 
-#create registry key named private and a dword named autosetup and set it to 0 to prevent windows from setting up network devices
-subprocess.call(r"powershell.exe New-Item -Path 'hklm:\SOFTWARE\Microsoft\Windows\CurrentVersion\NcdAutoSetup' -Name 'Private'")
-subprocess.call(r"powershell.exe New-ItemProperty 'hklm:\SOFTWARE\Microsoft\Windows\CurrentVersion\NcdAutoSetup\Private' -Name 'AutoSetup' -Value 0 -PropertyType 'DWord'")
+yn = input("Do you want to configure the network settings? Y/N: ")
+if yn.lower() == "y":
+    print('Configuring network settings')
+    definitions.network_settings()
 
-#turn off ipv6 for all network adapters
-subprocess.call(r"powershell.exe $nic=get-netadapter; Disable-NetAdapterBinding -Name $nic.name -ComponentID ms_tcpip6", shell=True)
-
-#set current network profile to private
-subprocess.call(r"powershell.exe $profile=get-netconnectionprofile; Set-NetConnectionProfile -InterfaceAlias $profile.InterfaceAlias -NetworkCategory Private", shell=True)
-
-#powershell commands to add firewall rules to allow network discovery and enable file and printer sharing
-subprocess.call(r"powershell.exe netsh advfirewall firewall set rule group='network discovery' new enable=Yes")
-subprocess.call(r"powershell.exe netsh advfirewall firewall set rule group='File and Printer Sharing' new enable=Yes")
-
-#key for turning off windows managing default printer
-subprocess.call(r"powershell.exe reg add 'HKEY_CURRENT_USER\Software\Microsoft\Windows NT\CurrentVersion\Windows' /v LegacyDefaultPrinterMode /t REG_DWORD /d 1 /f", shell=True)
-
-#powershell command to rename computer. uncomment when variable is setup with computer name
-current_computer_name=subprocess.call(r"powershell.exe [System.Net.Dns]::GetHostByName($env:computerName).hostname", shell=True)
-new_computer_name=input("Please enter the new Computer Name: ")
-
-
-subprocess.call(r"powershell.exe Rename-Computer -NewName "+new_computer_name,shell=True)
+else:
+    print("Skipping network configuration...")
 
 #powershell command to set protocol defaults and file type association defaults for target user. needs variable to hold username, and variable to hold password. dependent on setfta.ps1. must be ran AFTER user creation is completed
 #can be cleaned up using variables to hold commands in between semi-colons. 
 #currently sets chrome as default, adobe reader, and outlook for target user from admin context. will affect user, not admin.
 
-print("Do you want to create a user?")
-yn = input("Y/N: ")
-if yn == "Y" or "y":
+#        r"start-process powershell.exe -argumentlist 'import-module c:\util\project\setfta.ps1; set-pta chromehtml http; set-pta chromehtml https; set-fta AcroExch.Document.DC .pdf; set-pta Outlook.URL.mailto.15 mailto; set-fta chromehtml .html'"
+#        r" -Credential (New-Object System.Management.Automation.PSCredential " + str(current_computer_name)+"\\"+username+",$password)", shell=True)
 
-    subprocess.call(r"powershell.exe set-executionpolicy remotesigned")
-    current_computer_name=((subprocess.check_output(r"powershell.exe [System.Net.Dns]::GetHostByName($env:computerName).hostname", shell=True)).decode('utf-8')).strip()
-    fname=input("Please enter the user's first name: ")
-    lname=input("Please enter the user's last name: ")
-
-    lnamelist=list(lname)
-    lnamelist.insert(0, fname[0])
-
-    for i in range(len(lnamelist)):
-        lnamelist[i]=lnamelist[i].lower()
-
-    username="".join(map(str, lnamelist))
-    subprocess.call(r"powershell.exe Write-Host 'Please enter their password: '; $password=Read-Host -assecurestring; "
-        r"New-LocalUser "+ username + " -Password $password -FullName '" + fname + " " + lname + "'; Add-LocalGroupMember -Group 'Users' -Member " + username + ";"
-        r"start-process powershell.exe -argumentlist 'import-module c:\util\project\setfta.ps1; set-pta chromehtml http; set-pta chromehtml https; set-fta AcroExch.Document.DC .pdf; set-pta Outlook.URL.mailto.15 mailto; set-fta chromehtml .html'"
-        r" -Credential (New-Object System.Management.Automation.PSCredential " + str(current_computer_name)+"\\"+username+",$password)", shell=True)
-
-
-elif yn == "N" or "n":
-      print("User creation skipped")
+yn = input("Do you want to create a user? Y/N: ")
+if yn.lower() == "y":    
+    definitions.apply_defaults(definitions.create_user())
+    
 else:
-      print("Invalid Response, skipping user creation.")
+      print("Skipping user creation...")
+
---
+++ definitions.py
@@ -0,0 +1,105 @@
+import subprocess
+import os
+import glob
+from getpass import getpass
+from winreg import *
+
+
+def agent_install():
+    folder="C:\Program Files (x86)\ScreenConnect Client*"
+    if not glob.glob(folder,recursive=False):
+        os.system(r"msiexec /i C:\UTIL\Project\Agent_Install.msi /qb") 
+        
+def bitdefender_install():
+    folder="C:\Program Files\Bitdefender"
+    if not os.path.isdir(folder):
+        os.system(r"msiexec /i C:\UTIL\Project\eps_installer_signed.msi /qb GZ_PACKAGE_ID=aHR0cHM6Ly9jbG91ZC1lY3MuZ3Jhdml0eXpvbmUuYml0ZGVmZW5kZXIuY29tL1BhY2thZ2VzL0JTVFdJTi8wL0RlUkQ0WS9pbnN0YWxsZXIueG1sP2xhbmc9ZW4tVVM=")
+
+def applications_install():
+    folder="C:\Program Files\WindowsApps\Microsoft.VCLibs*"
+    
+    if not glob.glob(folder,recursive=False):
+        subprocess.call(r"powershell.exe Add-AppPackage -Path 'C:\UTIL\Project\Microsoft.VCLibs.x64.14.00.Desktop.appx'", shell=True)
+    #output_arr = []
+    ##output_arr.append(subprocess.check_output(r"powershell.exe get-appxpackage Microsoft.VCLibs.140.00.UWPDesktop", shell=True))
+    #output_arr.append(subprocess.check_output(r"powershell.exe get-appxpackage Microsoft.VCLibs.140.00", shell=True))
+
+    #for i in output_arr:
+    #    if not i.decode('utf-8'):
+    #        subprocess.call(r"powershell.exe Add-AppPackage -Path 'C:\UTIL\Project\Microsoft.VCLibs.x64.14.00.Desktop.appx'", shell=True)
+
+    subprocess.call(r"powershell.exe Add-AppPackage -Path 'C:\UTIL\Project\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'", shell=True)
+    subprocess.call(r"powershell.exe winget install --id=Oracle.JavaRuntimeEnvironment; winget install --id=Google.Chrome; winget install --id=Mozilla.Firefox; winget install --id=Microsoft.RemoteDesktopClient; winget install --id=Adobe.AdobeAcrobatReaderDC; winget install --id=JAMSoftware.TreeSizeFree", shell=True)
+
+def office_install():
+    os.system(r"C:\Util\Project\setup.exe /configure C:\UTIL\Project\Configuration_test.xml")
+    
+def network_settings():
+    #create registry key named private and a dword named autosetup and set it to 0 to prevent windows from setting up network devices
+    subprocess.call(r"powershell.exe New-Item -Path 'hklm:\SOFTWARE\Microsoft\Windows\CurrentVersion\NcdAutoSetup' -Name 'Private'")
+    subprocess.call(r"powershell.exe New-ItemProperty 'hklm:\SOFTWARE\Microsoft\Windows\CurrentVersion\NcdAutoSetup\Private' -Name 'AutoSetup' -Value 0 -PropertyType 'DWord'")
+
+    #turn off ipv6 for all network adapters
+    subprocess.call(r"powershell.exe $nic=get-netadapter; Disable-NetAdapterBinding -Name $nic.name -ComponentID ms_tcpip6", shell=True)
+
+    #set current network profile to private
+    subprocess.call(r"powershell.exe $profile=get-netconnectionprofile; Set-NetConnectionProfile -InterfaceAlias $profile.InterfaceAlias -NetworkCategory Private", shell=True)
+
+    #powershell commands to add firewall rules to allow network discovery and enable file and printer sharing
+    subprocess.call(r"powershell.exe netsh advfirewall firewall set rule group='network discovery' new enable=Yes")
+    subprocess.call(r"powershell.exe netsh advfirewall firewall set rule group='File and Printer Sharing' new enable=Yes")
+
+    #key for turning off windows managing default printer
+    subprocess.call(r"powershell.exe reg add 'HKEY_CURRENT_USER\Software\Microsoft\Windows NT\CurrentVersion\Windows' /v LegacyDefaultPrinterMode /t REG_DWORD /d 1 /f", shell=True)
+
+def computer_rename():
+    #powershell command to rename computer. uncomment when variable is setup with computer name
+    #current_computer_name=subprocess.call(r"powershell.exe [System.Net.Dns]::GetHostByName($env:computerName).hostname", shell=True)
+    new_computer_name=input("Please enter the new Computer Name: ")
+    subprocess.call(r"powershell.exe Rename-Computer -NewName "+new_computer_name,shell=True)
+
+def create_user():
+    subprocess.call(r"powershell.exe Set-ExecutionPolicy RemoteSigned -Scope LocalMachine -Force")
+    fname=input("Please enter the user's first name: ")
+    lname=input("Please enter the user's last name: ")
+
+    lnamelist=list(lname)
+    lnamelist.insert(0, fname[0])
+
+    for i in range(len(lnamelist)):
+        lnamelist[i]=lnamelist[i].lower()
+
+    username="".join(map(str, lnamelist))
+    pass_key=getpass("Please enter the user's password: ")
+    subprocess.call(r"powershell.exe New-LocalUser "+ username + " -Password (ConvertTo-SecureString " +pass_key+ " -AsPlainText -Force) -FullName '" + fname + " " + lname + "'; Add-LocalGroupMember -Group 'Users' -Member " + username + ";",shell=True)
+    return username,pass_key
+
+def apply_defaults(username):
+    current_computer_name=((subprocess.check_output(r"powershell.exe [System.Net.Dns]::GetHostByName($env:computerName).hostname", shell=True)).decode('utf-8')).strip()
+    action=r"(New-ScheduledTaskAction -execute powershell.exe -Argument c:\util\project\defaultsetup.ps1)"
+    trigger=r"(New-ScheduledTaskTrigger -atlogon)"
+    credentials=r"-Credential (New-Object System.Management.Automation.PSCredential " +str(current_computer_name)+"\\"+str(username[0])+",(ConvertTo-SecureString " +str(username[1])+ " -AsPlainText -Force))"
+    principal=r"(New-ScheduledTaskPrincipal -UserId "+username[0]+" -LogonType ServiceAccount)"
+    arguments=r"register-scheduledtask -action "+action+" -trigger "+trigger+" -principal "+principal+" -taskname pls -taskpath defaults -description fta_scheduled_task"#; get-scheduledtask pls"
+    
+    subprocess.call(r"powershell.exe start-process powershell.exe -argumentlist '"+arguments+"' ", shell=True)
+    
+    #aKey = r"SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList"
+    #aReg = ConnectRegistry(None, HKEY_LOCAL_MACHINE)
+    #store1 = 0
+    #store2 = 0
+    #aKey = OpenKey(aReg, aKey)
+    #for i in range(10):
+    #    asubkey_name = EnumKey(aKey, i)
+    #    print(asubkey_name)
+    #    asubkey = OpenKey(aKey, asubkey_name)
+    #    print(asubkey)
+    #    val = QueryValueEx(asubkey, r"ProfileImagePath")
+    #    store = str(val)
+    #    if username in store:
+    #        store2 = str(asubkey_name)
+
+    #subprocess.call("powershell.exe REG LOAD HKEY_USERS\\" +store2+ r" 'C:\Users\\" +username+ r"NTUSER.DAT'; " + "New-Item 'Registry::HKEY_USERS\\" + store2 + r"\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce'")
+    #subprocess.call("powershell.exe REG LOAD HKEY_USERS\\" +store2+ r" 'C:\Users\\" +username+ r"NTUSER.DAT'; " + "Set-ItemProperty 'Registry::HKEY_USERS\\" + store2 + r"\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce' -Name 'DefaultSetup' -Value 'C:\UTIL\Project\defaultsetup.ps1' -Type 'String'")
+    #subprocess.call("powershell.exe REG UNLOAD HKEY_USERS\\" +store2)
+    
