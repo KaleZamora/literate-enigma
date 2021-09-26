638770d5aed7ccced2f20e8482ef07f1b9dcdb8d
boomearth <gherrera@managedservicesit.com>
2021-09-26 18:11:44 -0400

    sync'd to upstream repo so nothing is deleted

---
+++ InstallScript.py
@@ -0,0 +1,66 @@
+import definitions
+import ctypes
+import sys
+
+sys.excepthook = definitions.myexcepthook
+
+if definitions.is_admin():
+    value = definitions.menu()
+
+    if value == "1":
+        definitions.office_install()
+
+    elif value == "2":
+        definitions.agent_install()    
+
+    elif value == "3":
+        definitions.bitdefender_install()
+
+    elif value == "4":
+        definitions.applications_install()
+      
+    elif value == "5":
+        definitions.applications_install()
+        definitions.bitdefender_install()
+        definitions.office_install()
+        definitions.agent_install()
+      
+    elif value == "6":
+        definitions.bitdefender_install()
+        definitions.agent_install()
+    
+    elif value == "7":
+        print("Goodbye!")
+        exit()
+      
+    elif value == "8":
+        print("testing configurations")
+
+    elif value == "9":
+        definitions.applications_install()
+        definitions.office_install()
+      
+    #Network configuration
+    definitions.netmenu()
+
+    #Power settings configuration
+    definitions.powermenu()
+
+    #powershell command to set protocol defaults and file type association defaults for target user. needs variable to hold username, and variable to hold password. dependent on setfta.ps1. must be ran AFTER user creation is completed
+    #can be cleaned up using variables to hold commands in between semi-colons. 
+    #currently sets chrome as default, adobe reader, and outlook for target user from admin context. will affect user, not admin.
+
+    #        r"start-process powershell.exe -argumentlist 'import-module c:\util\project\setfta.ps1; set-pta chromehtml http; set-pta chromehtml https; set-fta AcroExch.Document.DC .pdf; set-pta Outlook.URL.mailto.15 mailto; set-fta chromehtml .html'"
+    #        r" -Credential (New-Object System.Management.Automation.PSCredential " + str(current_computer_name)+"\\"+username+",$password)", shell=True)
+
+    #User Creation
+    definitions.usermenu()
+
+    #Computer rename
+    definitions.comp_name_menu()
+
+    definitions.restart_menu()
+
+else:
+    # Re-run the program with admin rights
+    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv[1:]), None, 1)
